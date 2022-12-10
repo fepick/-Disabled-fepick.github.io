5ce28958e0c821a58a182ebf48b279ba0e40ddd5
fepick <93140874+fepick@users.noreply.github.com>
2022-12-10 15:07:34 +0900

    Update main.js
    restore hide and show

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 function hide_three_content(box){
     if(box.checked == true){
+        document.getElementById("canvas1").style.display = "none"
         document.getElementById("wrap_three").style.display = "none"
         document.getElementById("hide_label").innerText = "show"
     }
     else {
+        document.getElementById("canvas1").style.display = "block"
         document.getElementById("wrap_three").style.display = "block"
         document.getElementById("hide_label").innerText = "hide"
     }
